44a9f650554752fcbf0730c6495e62f5925edd28
refaamirul <andimochrefa@gmail.com>
2023-06-08 09:38:28 +0800

    portfolio

--- src/assets/components/Portfolio/Portfolio.jsx
+++ src/assets/components/Portfolio/Portfolio.jsx
@@ -24,6 +24,28 @@ const Portfolio = () => {
             <p className="text-base font-medium">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi, iusto sint. Facere expedita provident magni sequi, quos, saepe exercitationem odio illum aperiam accusantium sunt quod, aliquid amet optio aut? Nemo, animi est placeat voluptatem inventore a mollitia quisquam! Delectus quasi qui nobis ratione inventore temporibus explicabo dolorum perferendis fugiat nihil.</p>
           </div>
         </div>
+        <div className="flex w-full justify-center px-4">
+          <div className="mb-12 p-4">
+            <div className="overflow-hidden rounded-md shadow-md">
+              <a href="https://amazon-youtube.pages.dev/">
+                <img src={Project1} width={200} alt="" />
+              </a>
+            </div>
+            <h3 className="mt-5 mb-3 font-semibold">Project 2</h3>
+            <p className="text-base font-medium">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi, iusto sint. Facere expedita provident magni sequi, quos, saepe exercitationem odio illum aperiam accusantium sunt quod, aliquid amet optio aut? Nemo, animi est placeat voluptatem inventore a mollitia quisquam! Delectus quasi qui nobis ratione inventore temporibus explicabo dolorum perferendis fugiat nihil.</p>
+          </div>
+        </div>
+        <div className="flex w-full justify-center px-4">
+          <div className="mb-12 p-4">
+            <div className="overflow-hidden rounded-md shadow-md">
+              <a href="https://amazon-youtube.pages.dev/">
+                <img src={Project1} width={200} alt="" />
+              </a>
+            </div>
+            <h3 className="mt-5 mb-3 font-semibold">Project 3</h3>
+            <p className="text-base font-medium">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi, iusto sint. Facere expedita provident magni sequi, quos, saepe exercitationem odio illum aperiam accusantium sunt quod, aliquid amet optio aut? Nemo, animi est placeat voluptatem inventore a mollitia quisquam! Delectus quasi qui nobis ratione inventore temporibus explicabo dolorum perferendis fugiat nihil.</p>
+          </div>
+        </div>
       </div>
     </section>
   )
